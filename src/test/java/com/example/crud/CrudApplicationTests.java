b3b067ed2d1d6154d7f5a982e4ba93d2fbe69b24
Fernanda Kipper <nanda.kipper@gmail.com>
2023-05-25 14:23:37 -0300

    feat: initial config

---
+++ src/test/java/com/example/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
