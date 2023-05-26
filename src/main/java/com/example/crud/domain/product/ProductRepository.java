304d042e314ba0bb5fe70017b91ea01054415187
Fernanda Kipper <nanda.kipper@gmail.com>
2023-05-25 23:48:52 -0300

    feat: create ProductRepository

---
+++ src/main/java/com/example/crud/domain/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.crud.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, String> {}
