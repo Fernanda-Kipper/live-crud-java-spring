86df70aa744f382972e088f75f25bf277597f410
Fernanda Kipper <nanda.kipper@gmail.com>
2023-05-25 23:48:22 -0300

    feat: create Product entity

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,27 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name="product")
+@Entity(name="product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price_in_cents;
+
+    public Product(RequestProduct requestProduct){
+        this.name = requestProduct.name();
+        this.price_in_cents = requestProduct.price_in_cents();
+    }
+}
